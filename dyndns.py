6a5c4c2927dfa2c814587af9bd8f35518bea4064
florianschleuss <florian.schleuss@gmx.net>
2022-04-02 11:34:15 +0200

    Conversion to cloudflare
    Signed-off-by: florianschleuss <florian.schleuss@gmx.net>

--- dyndns.py
+++ dyndns.py
@@ -2,15 +2,14 @@ import requests
 import json
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 from flask import Flask, jsonify, request
-# from flask_httpauth import HTTPBasicAuth
 from flask_restful import Api
 
 load_dotenv()
-USERNAME=os.environ.get("USERNAME")
-PASSWORD=os.environ.get("PASSWORD")
-DOMAIN=os.environ.get("DOMAIN")
+API_TOKEN = os.environ.get("API_TOKEN")
+ZONE_ID = os.environ.get("ZONE_ID")
 
 # List of subdomains to update. '' for root domain
 SUBDOMAINS = [i for i in os.environ.get("SUBDOMAINS").split(", ")]
@@ -20,88 +19,76 @@ IPv6=''
 IP=''
 
 
-
-# Will create a requests session and log you into your one.com account in that session
-def _loginSession(USERNAME, PASSWORD, DOMAIN):
-
-    print('Logging in', flush=True)
-
-    loginurl = 'https://www.one.com/admin/login.do'
-
-    session = requests.session()
-    logindata = {'loginDomain': True,'displayUsername': USERNAME, 'password1': PASSWORD, 'username': USERNAME, 'targetDomain': '', 'loginTarget': ''}
-    session.post(loginurl, data=logindata)
-    if session.get('https://www.one.com/admin/api/domains/' + DOMAIN + '/dns/custom_records').status_code != 200:
-        raise Exception('Login failed')
-
-    return session
-
-
 # Gets all DNS records on your domain.
-def _getCustomRecords(session, DOMAIN):
-    print('Getting Records', flush=True)
-    return json.loads(session.get('https://www.one.com/admin/api/domains/' + DOMAIN + '/dns/custom_records').text)['result']['data']
-
-
-# Finds the record id of a record from it's subdomain
-def _findIdBySubdomain(records, subdomain, rc_type='A'):
-    for obj in records:
-        if obj['attributes']['prefix'] == subdomain and obj['attributes']['type'] == rc_type:
-            print('Found', rc_type, 'domain "' + subdomain + '": ' + obj['id'], flush=True)
-            return obj['id']
-    print('No ID found for "' + subdomain + '"', rc_type, flush=True)
-    return ''
+def _getCustomRecords():
+    resp = requests.get(
+        'https://api.cloudflare.com/client/v4/zones/{}/dns_records'.format(ZONE_ID),
+        headers={
+            'Authorization': f'Bearer {API_TOKEN}',
+        })
+    return resp.json()['result']
 
 
 # changes the IP Address of a TYPE A record. Default TTL=3800
-def _changeIP(session, records, DOMAIN, SUBDOMAIN, IP, IPv6, TTL=3800):
+def _changeIP(records, SUBDOMAIN, IP, IPv6):
+    for rec in records:
+        if SUBDOMAIN not in rec['name']:
+            continue
+        type = rec['type']
 
-    
-    basednsurl='https://www.one.com/admin/api/domains/' + DOMAIN + '/dns/custom_records/'
-    sendheaders={'Content-Type': 'application/json'}
+        if type == 'AAAA':
+            IP = IPv6
 
-    v4 = {'type':'dns_service_records','id':_findIdBySubdomain(records, SUBDOMAIN),'attributes':{'type':'A','prefix':SUBDOMAIN,'content':IP,'priority': 1, 'ttl':TTL}}
-    ret = session.patch(basednsurl + v4['id'], data=json.dumps(v4), headers=sendheaders)
-    if ret.status_code != 200:
-        print('IPv4 update failed', flush=True)
-        print(ret.text, flush=True)
-    else:
-        print('IPv4 change successful on subdomain "' + SUBDOMAIN + '" to new IP "' + v4['attributes']['content'] + '"', flush=True)
-    
-    v6 = {'type':'dns_service_records','id':_findIdBySubdomain(records, SUBDOMAIN, 'AAAA'),'attributes':{'type':'AAAA','prefix':SUBDOMAIN,'content':IPv6,'ttl':TTL}}
-    ret = session.patch(basednsurl + v6['id'], data=json.dumps(v6), headers=sendheaders)
-    if ret.status_code != 200:
-        print('IPv6 change failed\n', flush=True)
-        print(ret.text, flush=True)
-    else:
-        print('IPv6 change successful on subdomain "' + SUBDOMAIN + '" to new IP "' + v6['attributes']['content'] + '"\n', flush=True)
+        resp = requests.put(
+            'https://api.cloudflare.com/client/v4/zones/{}/dns_records/{}'.format(
+                ZONE_ID, rec['id']),
+            json={
+                'type': type,
+                'name': rec['name'],
+                'content': IP,
+                'proxied': False
+            },
+            headers={
+                'Authorization': f'Bearer {API_TOKEN}',
+            })
+        if resp.status_code != 200:
+            print(f'{type} update failed', flush=True)
+            print(resp.text, flush=True)
+        else:
+            print(
+                f'{type} change successful on subdomain "' + SUBDOMAIN +
+                '" to new IP "' + IP + '"', flush=True)
     return
 
+
 def _update_dns():
+    # List of subdomains to update. '' for root domain
+    SUBDOMAINS = [i for i in os.environ.get("SUBDOMAINS").split(", ")]
     try:
-        IPv6=requests.get('https://api64.ipify.org/').text
-        IP=requests.get('https://api.ipify.org/').text
+        IPv6 = requests.get('https://api64.ipify.org/').text
+        IP = requests.get('https://api.ipify.org/').text
     except Exception:
         print('Error while IP detection', flush=True)
-    # Create login session
-    s = _loginSession(USERNAME, PASSWORD, DOMAIN)
+        return
 
     # get dns records
-    records = _getCustomRecords(s, DOMAIN)
+    records = _getCustomRecords()
 
     # loop through list of subdomains
     for subdomain in SUBDOMAINS:
-        _changeIP(s, records, DOMAIN, subdomain, IP, IPv6)
+        _changeIP(records, subdomain, IP, IPv6)
+
 
 app = Flask(__name__)
 api = Api(app)
-@app.route('/dyndns/update_dns', methods=['GET'])
+@app.route('/update_dns', methods=['GET'])
 def update_dns():
     _update_dns()
     return 'success', 200
- 
+
 if __name__ == '__main__':
     # _update_dns()
 
-    app.run(host='0.0.0.0', port=80, debug=True)
+    print(datetime.now())
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
 
--- setup.DE.md
+++ setup.DE.md
@@ -2,6 +2,11 @@
 Auf dem Gerät:
 1. dyndns.py wie in der Anleitung per CRONJOB automatisch starten lassen:
    https://www.instructables.com/Raspberry-Pi-Launch-Python-script-on-startup/
+   1. chmod 755 dyndns.sh
+   2. sh dyndns.sh
+   3. sudo pip3 install flask flask_restful
+   4. sudo crontab -e
+   5. @reboot sh /home/pi/dyndns-script/dyndns.sh >/home/pi/dyndns-script/cronlog 2>&1
 2. Empfehlung: Logdatei mit anlegen. Vereinfacht Debugging.
 3. Nach der Erstellung reboot. 
 
